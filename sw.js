2d18a8de74174c73939cadf16ceb02fd33610707
jojiiiiyoung <jojiiiiyoung@gmail.com>
2023-01-27 15:50:27 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
 self.addEventListener("install", e => {
   console.info("Service Worker Installed")
 })
+
+// self.addEventListener("fetch", e => {
+//   console.info(e.request.url)
+//   e.respondWith(
+//     caches.match(e.request).then(response => response || fetch(e.request))
+//   )
+// })
